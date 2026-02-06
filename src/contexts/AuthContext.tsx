e2f29026cfba25e1ea7a844065e2e2ce59eff786
digpalsinghpanwar <digpal.singh@kenpath.io>
2026-02-06 14:19:12 +0530

    updating pulic key

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -57,14 +57,14 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const [publicKey, setPublicKey] = useState<CryptoKey | null>(null);
 
   // JWT validation public key
-  const publicKeyPEM = `-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAu1SU1LfVLPHCozMxH2Mo
-4lgOEePzNm0tRgeLezV6ffAt0gunVTLw7onLRnrq0/IzW7yWR7QkrmBL7jTKEn5u
-+qKhbwKfBstIs+bMY2Zkp18gnTxKLxoS2tFczGkPLPgizskuemMghRniWaoLcyeh
-kd3qqGElvW/VDL5AaWTg0nLVkjRo9z+40RQzuVaE8AkAFmxZzow3x+VJYKdjykkJ
-0iT9wCS0DRTXu269V264Vf/3jvredZiKRkgwlL9xNAwxXFg0x/XFw005UWVRIkdg
-cKWTjpBP2dPwVZ4WWC+9aGVd+Gyn1o0CLelf4rEjGoXbAAEgAqeGUxrcIlbjXfbc
-mwIDAQAB
+  const publicKeyPEM = `------BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAkvyeaWfmnLrbNneMjJ16
++FeHBSAeheTiaUWGidoBI4sYEHxB3rGlr+7WGMyX4rmfFCUDnCIWGuKt32UoA9CZ
+mgE9JCbmJLM1dR35cN9yEUmXggYXRJB8pMqlt+u3jHRFieLumzk1keEiTCsQqvgs
+txlhdBHyPTo7lAcaeFWgoK1CjqDi9xlZuTNUQB8WqhhCtjiEjE1Vj/G6DzYPcJ/g
+eJNM7/Dku5awXwG7lGqjKGuXj+C9fDF/zXrXAhGSVuSMW2hYczmILDyKaes2iH8K
+cYYzlCVV0lzJ+Sa98Fvpb/tOMY6XqoTzmkU/WlRoYY7jsqFykAcbOpncyO+lm+WW
+rQIDAQAB
 -----END PUBLIC KEY-----`;
 
   // Initialize auth state on component mount
